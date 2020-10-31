aba2d54b4ccebe82a85f1d499266033c5d364eb3
dantedxwolf <teacher.martin@gmail.com>
2020-10-31 09:53:36 +0600

    ready to deploy

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [iframes, setIframes] = useState(undefined);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const searchStringArray = e.target.searchString.value.split(" ");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const body = { searchStringArray };
+    try {
+      const response = await axios.post(
+        "/sendSearchStringArray",
+        JSON.stringify(body),
+        config
+      );
+      const iframes1 = response.data.map((eachUrl) => {
+        let srcString = `//www.youtube.com/embed/${eachUrl}`;
+        return (
+          <iframe
+            key={Math.random()}
+            title={Math.random().toString()}
+            src={srcString}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        );
+      });
+      setIframes(iframes1);
+      alert('Please wait until all the videos are finished loading!')
+    } catch (err) {
+      console.log(err);
+    }
+    e.target.searchString.value = "";
+  };
+    
+  
+  
+  return (
+    <div className="App">
+      <h1 style={{color:'white',textAlign:'center'}}>Welcome you Devil May Cry Video Service</h1>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <input type="text" required id="searchString" name="searchString" placeholder="Search" />
+        <button type="Search">Search</button>
+      </form>
+      <div className="videos">
+        {iframes === undefined ? <p></p> : iframes }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ routes/sendSearchStringArray.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const axios = require('axios');
+
+router.post("/", async (req, res) => {
+  const searchStringArray = req.body.searchStringArray;
+  let searchString = "";
+  if (searchStringArray.length === 0) {
+    searchString += searchStringArray[0];
+  } else {
+    searchStringArray.map((eachElement, index) => {
+      if (index === searchStringArray.length - 1) {
+        searchString += eachElement;
+      } else {
+        searchString += eachElement + "%20";
+      }
+    });
+  }
+//   console.log(searchString);
+
+  const endPoint1 =
+    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=40&q=" +
+    searchString +
+    "&key=AIzaSyBmWe2xrHIzw8LjOMyo16Ho4RaU8-EVa34";
+//   console.log(endPoint1);
+  try {
+    const response = await axios.get(endPoint1);
+    const data = response.data;
+    // console.log(data.items);
+     //this is the array of the items
+    let videoIds = data.items.map(eachItem => {
+        if(eachItem.id.videoId !== null || eachItem.id.videoId !== undefined ){
+            return eachItem.id.videoId;
+        }
+    });
+    videoIds = videoIds.filter(each => each !== undefined);
+    res.status(200).json(videoIds);
+
+  } catch (err) {
+    res.send("There was an error with the api");
+  }
+
+});
+
+module.exports = router;
